2886ec44f0c92223e1be1bf35fd4be3c7ceabebc
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-10 12:30:00 +0000

    Creating first express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
+
+// req is the Request object, res is the Response object
+// (these are just variable names, they can be anything but it's a convention to call them req and res)
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
