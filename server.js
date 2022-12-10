692ef19be1d778985c2cb0084b54d2cdb5e62bcb
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-10 13:18:16 +0000

    app.get()

--- server.js
+++ server.js
@@ -8,5 +8,17 @@ app.listen(3000, function () {
 // req is the Request object, res is the Response object
 // (these are just variable names, they can be anything but it's a convention to call them req and res)
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.send("Hello world!");
+});
+
+app.get("/node", function (req, res) {
+  res.send("This is the node response");
+});
+
+app.get("/chocolate", function (req, res) {
+  res.send("Mm chocolate :O");
+});
+
+app.get("/codeyourfuture", function (req, res) {
+  res.send("CYF is great to learn many things!");
 });
